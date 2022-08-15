fce748d848b21e14e6cdbd185bee75b72ae33c8a
Emilly Estefanny <emilly.e@escolar.ifrn.edu.br>
2022-08-15 09:28:34 -0300

    commit fofo

---
+++ 01_quadrado.py
@@ -0,0 +1,9 @@
+import turtle
+
+turtle = turtle.Turtle()
+turtle.shape('turtle')
+turtle.pensize(5)
+
+for c in range(4):
+    turtle.forward(100)
+    turtle.left(90)
---
+++ 02_diamante.py
@@ -0,0 +1,16 @@
+import turtle 
+
+turtle = turtle.Turtle()
+turtle.shape('turtle')
+turtle.pensize(5)
+
+for c in range(2):
+    turtle.left(60)
+    turtle.forward(150)
+turtle.left(120)
+turtle.forward(150)
+turtle.left(60)
+turtle.forward(150)
+
+    
+    
